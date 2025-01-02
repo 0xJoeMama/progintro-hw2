945f60c2412b1aa2bd2a81043372e5c37fdd9c26
0xJoeMama <0xjoemama@gmail.com>
2025-01-02 12:25:46 +0200

    fix: printing to stderr

--- future/src/future.c
+++ future/src/future.c
@@ -3,24 +3,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+static inline int print_usage(const char *prog_name) {
+  fprintf(stderr, "Usage: %s <filename> [--window N (default: 50)]\n",
+          prog_name);
+  return 1;
+}
+
 int main(int argc, const char **argv) {
   if (argc < 2 || argc > 4) {
-    printf("Usage: %s <filename> [--window N (default: 50)]\n", argv[0]);
-    return 1;
+    return print_usage(argv[0]);
   }
 
   const char *filename = NULL;
   long long window_sz = 50;
   for (int i = 1; i < argc; i++) {
     if (strncmp("--window", argv[i], 8) == 0) {
-      if (i + 1 >= argc) {
-        fprintf(stderr, "expected an integer as window size");
-        return 1;
+      if (i + 1 >= argc)
+        return print_usage(argv[0]);
+
+      char* end;
+      window_sz = strtoull(argv[i + 1], &end, 10);
+
+      if (end == argv[i+1])  {
+        return print_usage(argv[0]);
       }
 
-      window_sz = strtoull(argv[i + 1], NULL, 10);
       if (errno != 0) {
-        perror("could not parse window size: ");
+        perror("could not parse window size");
         return 1;
       }
       i++;
@@ -29,10 +38,8 @@ int main(int argc, const char **argv) {
     }
   }
 
-  if (!filename) {
-    fprintf(stderr, "filename was not provided\n");
-    return 1;
-  }
+  if (!filename)
+    return print_usage(argv[0]);
 
   if (window_sz <= 0) {
     fprintf(stderr, "Window too small!\n");
@@ -47,18 +54,21 @@ int main(int argc, const char **argv) {
 
   FILE *value_file = fopen(filename, "r");
   if (!value_file) {
-    perror("could not open value file: ");
+    perror("could not open value file");
+    free(window);
     return 1;
   }
 
-  long long nums_read = 0;
+  size_t nums_read = 0;
   while (fscanf(value_file, "%lf", &window[nums_read % window_sz]) == 1)
     nums_read++;
 
-  // TODO: handle error case
-  fclose(value_file);
+  if (fclose(value_file) != 0) {
+    perror("could not close value file");
+    return 1;
+  }
 
-  if (window_sz > nums_read) {
+  if ((size_t)window_sz > nums_read) {
     fprintf(stderr, "Window too large!\n");
     free(window);
     return 1;
