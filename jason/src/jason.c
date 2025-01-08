74e201a65bae13f6023c4ad1f95ab6cf1aee53cd
0xJoeMama <0xjoemama@gmail.com>
2025-01-08 22:49:11 +0200

    feat: jason parser

--- jason/src/jason.c
+++ jason/src/jason.c
@@ -1,4 +1,10 @@
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define STRING_IMPL
@@ -15,18 +21,18 @@ typedef enum {
   JSON_OBJECT
 } JsonType_t;
 
-typedef union JsonElement JsonElement_t;
-typedef struct TaggedJsonElement TaggedJsonElement_t;
+typedef union JsonValue JsonValue_t;
+typedef struct TaggedJsonValue TaggedJsonValue_t;
 
-DA_DECLARE(TaggedJsonElement_t)
-HM_DECLARE(String_t, TaggedJsonElement_t)
+DA_DECLARE(TaggedJsonValue_t)
+HM_DECLARE(String_t, TaggedJsonValue_t)
 
 // an object is just a hashmap of elements
-typedef HashMap_t(String_t, TaggedJsonElement_t) JsonObject_t;
+typedef HashMap_t(String_t, TaggedJsonValue_t) JsonObject_t;
 // an array is just a dynamic array of elements
-typedef DynamicArray_t(TaggedJsonElement_t) JsonArray_t;
+typedef DynamicArray_t(TaggedJsonValue_t) JsonArray_t;
 
-union JsonElement {
+union JsonValue {
   String_t string;
   double number;
   bool boolean;
@@ -34,17 +40,513 @@ union JsonElement {
   JsonObject_t object;
 };
 
-struct TaggedJsonElement {
-  JsonElement_t el;
+struct TaggedJsonValue {
+  JsonValue_t el;
   JsonType_t type;
 };
 
-HM_IMPL(String_t, TaggedJsonElement_t)
-DA_IMPL(TaggedJsonElement_t)
+HM_IMPL(String_t, TaggedJsonValue_t)
+DA_IMPL(TaggedJsonValue_t)
 
-int parse_element(Str_t s, TaggedJsonElement_t *el) {
-  memset(el, 0, sizeof(TaggedJsonElement_t));
+int ss_advance_once(Str_t *s) {
+  if (s->len == 0)
+    return -1;
+
+  s->len--;
+  return *s->s++;
+}
+
+int ss_starts_with(Str_t s1, Str_t s2) {
+  if (s1.len < s2.len)
+    return 0;
+
+  while (s2.len > 0)
+    if (ss_advance_once(&s2) != ss_advance_once(&s1))
+      return 0;
+
   return 1;
 }
 
-int main() { return 0; }
+Str_t ss_advance(Str_t *s, size_t adv) {
+  if (s->len < adv)
+    return (Str_t){.len = 0, .s = NULL};
+
+  Str_t res = {.len = adv, .s = s->s};
+  s->s += adv;
+  s->len -= adv;
+
+  return res;
+}
+
+void json_deinit(TaggedJsonValue_t value);
+
+void json_object_kv_deinit(KVPair_t(String_t, TaggedJsonValue_t) entry) {
+  s_deinit(&entry.k);
+  json_deinit(entry.v);
+}
+
+void json_deinit(TaggedJsonValue_t value) {
+  switch (value.type) {
+  case JSON_NULL:
+  case JSON_BOOL:
+  case JSON_NUMBER:
+    break;
+  case JSON_STRING:
+    s_deinit(&value.el.string);
+    break;
+  case JSON_ARRAY:
+    da_deinit(TaggedJsonValue_t)(&value.el.array, json_deinit);
+    break;
+  case JSON_OBJECT:
+    hm_deinit(String_t, TaggedJsonValue_t)(&value.el.object,
+                                           json_object_kv_deinit);
+    break;
+  }
+}
+
+int handle_escape_sequence(String_t *result, Str_t *s) {
+  char unicode_buf[5] = {0};
+  int escaped_char = ss_advance_once(s);
+  if (escaped_char < 0)
+    return 0;
+
+  switch (escaped_char) {
+  case '"':
+  case '\\':
+  case '/':
+    return s_push(result, escaped_char);
+  case 'b':
+    return s_push(result, '\b');
+  case 'f':
+    return s_push(result, '\f');
+  case 'n':
+    return s_push(result, '\n');
+  case 'r':
+    return s_push(result, '\r');
+  case 't':
+    return s_push(result, '\t');
+  case 'u':
+    for (int i = 0; i < 4; i++) {
+      int next_char = ss_advance_once(s);
+      if (next_char < 0)
+        return 0;
+
+      // if any of the digits isn't a hex digit, we fail
+      if (!isxdigit(next_char))
+        return 0;
+
+      unicode_buf[i] = next_char;
+    }
+
+    // increment by the amount of characters we just read
+    unicode_buf[4] = '\0';
+    char *end;
+    errno = 0;
+    // out number has 4 digits meaning at most 16 binary digits
+    // so we just need to split the first 8 and the last 8 bits
+    int res = strtol(unicode_buf, &end, 16);
+    if (unicode_buf == end || errno)
+      return 0;
+
+    if (!s_push(result, (char)res >> 8))
+      return 0;
+
+    if (!s_push(result, (char)res & 0xFF))
+      return 0;
+    return 1;
+  default:
+    return 0;
+  }
+}
+
+int json_parse_string(Str_t *s, TaggedJsonValue_t *el) {
+  *s = ss_trim_left(*s);
+
+  if (*s->s != '"' || s->len == 0)
+    return 0;
+
+  String_t *result = &el->el.string;
+
+  // TODO: maybe handle this using an enum to differentiate between errors
+  if (!s_init(result, 16))
+    return 0;
+
+  // skip the " character
+  ss_advance_once(s);
+
+  while (s->len > 0) {
+    int next_char = ss_advance_once(s);
+    if (next_char < 0)
+      break;
+
+    switch (next_char) {
+      // end of string
+    case '"':
+      el->type = JSON_STRING;
+      return 1;
+    case '\\':
+      // if we can't properly handle the escape sequence, we have a skill issue
+      // and fail
+      if (!handle_escape_sequence(result, s)) {
+        s_deinit(result);
+        return 0;
+      }
+      break;
+    default:
+      // otherwise just append to the end of the string
+      if (!s_push(result, next_char)) {
+        s_deinit(result);
+        return 0;
+      }
+    }
+  }
+
+  s_deinit(result);
+  return 0;
+}
+
+int json_parse_number(Str_t *s, TaggedJsonValue_t *el) {
+  char *null_terminated_buffer = calloc(s->len + 1, sizeof(char));
+  if (!null_terminated_buffer)
+    return 0;
+
+  memcpy(null_terminated_buffer, s->s, s->len * sizeof(char));
+
+  char *end;
+  errno = 0;
+  double res = strtod(null_terminated_buffer, &end);
+  if (end == null_terminated_buffer || errno) {
+    free(null_terminated_buffer);
+    return 0;
+  }
+
+  size_t advance_bytes = end - null_terminated_buffer;
+  ss_advance(s, advance_bytes);
+
+  el->type = JSON_NUMBER;
+  el->el.number = res;
+
+  free(null_terminated_buffer);
+
+  return 1;
+}
+
+int json_parse_boolean(Str_t *s, TaggedJsonValue_t *value) {
+  *s = ss_trim_left(*s);
+  Str_t true_string = ss_from_cstring("true");
+  Str_t false_string = ss_from_cstring("false");
+
+  if (ss_starts_with(*s, true_string)) {
+    value->type = JSON_BOOL;
+    value->el.boolean = true;
+    ss_advance(s, true_string.len);
+    return 1;
+  } else if (ss_starts_with(*s, false_string)) {
+    value->type = JSON_BOOL;
+    value->el.boolean = false;
+    ss_advance(s, false_string.len);
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int json_parse_null(Str_t *s, TaggedJsonValue_t *value) {
+  *s = ss_trim_left(*s);
+  Str_t null = ss_from_cstring("null");
+  if (!ss_starts_with(*s, null))
+    return 0;
+
+  ss_advance(s, null.len);
+  value->type = JSON_NULL;
+  return 1;
+}
+
+int json_parse_value(Str_t *s, TaggedJsonValue_t *value);
+
+int json_parse_array(Str_t *s, TaggedJsonValue_t *value) {
+  *s = ss_trim_left(*s);
+  if (s->len == 0 || *s->s != '[')
+    return 0;
+
+  JsonArray_t *result = &value->el.array;
+  if (!da_init(TaggedJsonValue_t)(result, 16))
+    return 0;
+
+  ss_advance_once(s);
+  while (s->len > 0) {
+    *s = ss_trim_left(*s);
+    // end of array
+    if (*s->s == ']') {
+      if (ss_advance_once(s) < 0)
+        break;
+
+      // only successfull case
+      value->type = JSON_ARRAY;
+      return 1;
+    }
+
+    TaggedJsonValue_t new_value;
+    if (!json_parse_value(s, &new_value))
+      break;
+
+    if (!da_push(TaggedJsonValue_t)(result, new_value))
+      break;
+
+    *s = ss_trim_left(*s);
+
+    // we need to support dangling commas and this code does just that. Notice
+    // that this is *NOT* standard JSON
+    if (*s->s == ',') {
+      // trash the comma
+      if (ss_advance_once(s) < 0)
+        break;
+      // otherwise, if the next character is not a closing bracket, we have
+      // failed
+    } else if (*s->s != ']') {
+      break;
+    }
+  }
+
+  da_deinit(TaggedJsonValue_t)(result, json_deinit);
+  return 0;
+}
+
+// djb2 hashing algorithm
+uint64_t s_hash(String_t *s) {
+  uint64_t hash = 5381;
+  Str_t s_slice = s_str(s);
+  int c;
+
+  while ((c = ss_advance_once(&s_slice)) >= 0)
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+  return hash;
+}
+
+int s_eq(String_t *a, String_t *b) {
+  if (a->len != b->len)
+    return 0;
+
+  for (size_t i = 0; i < a->len; i++)
+    if (a->buf[i] != b->buf[i])
+      return 0;
+
+  return 1;
+}
+
+int json_parse_object(Str_t *s, TaggedJsonValue_t *value) {
+  *s = ss_trim_left(*s);
+  if (s->len == 0 || *s->s != '{')
+    return 0;
+
+  JsonObject_t *result = &value->el.object;
+  if (!hm_init(String_t, TaggedJsonValue_t)(result, 16, s_hash, s_eq))
+    return 0;
+
+  ss_advance_once(s);
+  while (s->len > 0) {
+    *s = ss_trim_left(*s);
+    // end of array
+    if (*s->s == '}') {
+      ss_advance_once(s);
+      // only successfull case
+      value->type = JSON_OBJECT;
+      return 1;
+    }
+
+    TaggedJsonValue_t key;
+    json_parse_string(s, &key);
+    if (key.type != JSON_STRING)
+      break;
+
+    *s = ss_trim_left(*s);
+    if (ss_advance_once(s) != ':') {
+      s_deinit(&key.el.string);
+      break;
+    }
+
+    *s = ss_trim_left(*s);
+
+    TaggedJsonValue_t new_value;
+    if (!json_parse_value(s, &new_value)) {
+      s_deinit(&key.el.string);
+      break;
+    }
+
+    if (!hm_put(String_t, TaggedJsonValue_t)(result, key.el.string, new_value)) {
+      s_deinit(&key.el.string);
+      break;
+    }
+
+    *s = ss_trim_left(*s);
+
+    // we need to support dangling commas and this code does just that. Notice
+    // that this is *NOT* standard JSON
+    if (*s->s == ',') {
+      // trash the comma
+      if (ss_advance_once(s) < 0)
+        break;
+      // otherwise, if the next character is not a closing bracket, we have
+      // failed
+    } else if (*s->s != '}') {
+      break;
+    }
+  }
+
+  hm_deinit(String_t, TaggedJsonValue_t)(result, json_object_kv_deinit);
+  return 0;
+}
+
+int json_parse_value(Str_t *s, TaggedJsonValue_t *value) {
+  memset(value, 0, sizeof(TaggedJsonValue_t));
+
+  *s = ss_trim(*s);
+
+  if (json_parse_string(s, value)) {
+    return 1;
+  } else if (json_parse_number(s, value)) {
+    return 1;
+  } else if (json_parse_boolean(s, value)) {
+    return 1;
+  } else if (json_parse_null(s, value)) {
+    return 1;
+  } else if (json_parse_array(s, value)) {
+    return 1;
+  } else if (json_parse_object(s, value)) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int json_parse(Str_t s, TaggedJsonValue_t *value) {
+  if (!json_parse_value(&s, value))
+    return 0;
+
+  s = ss_trim(s);
+  return s.len == 0;
+}
+
+int print_usage(const char *prog) {
+  fprintf(stderr, "Usage: %s [--extract <filename> | --bot]\n", prog);
+  return 1;
+}
+
+int extract_file(const char *filename, TaggedJsonValue_t *value) {
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    perror("could not open extraction file");
+    return 0;
+  }
+
+  if (fseek(file, 0, SEEK_END) == 0) {
+    long file_sz = ftell(file);
+    rewind(file);
+
+    char *buf = calloc(file_sz, sizeof(char));
+    fread(buf, file_sz, 1, file);
+    if (ferror(file)) {
+      if (fclose(file) != 0) {
+        perror("could not close extraction file");
+        return 0;
+      }
+      return 0;
+    }
+
+    if (fclose(file) != 0) {
+      perror("could not close extraction file");
+      return 0;
+    }
+
+    Str_t json_data = {
+        .s = buf,
+        .len = file_sz,
+    };
+
+    if (!json_parse(json_data, value)) {
+      free(buf);
+      return 0;
+    }
+
+    free(buf);
+  }
+
+  return 1;
+}
+
+#ifndef JSON_TESTS
+int main(int argc, const char **argv) {
+  if (argc < 2 || argc > 3)
+    return print_usage(argv[0]);
+
+  if (strcmp(argv[1], "--extract") == 0) {
+    if (argc != 3)
+      return print_usage(argv[0]);
+
+    TaggedJsonValue_t value;
+
+    // TODO: cleanup json on failure cases
+    if (!extract_file(argv[2], &value))
+      return 1;
+
+    if (value.type != JSON_OBJECT)
+      return 1;
+    String_t key;
+    if (!s_init(&key, 7))
+      return 1;
+    s_push_cstr(&key, "choices");
+
+    TaggedJsonValue_t *choices =
+        hm_get(String_t, TaggedJsonValue_t)(&value.el.object, &key);
+    if (!choices || choices->type != JSON_ARRAY) {
+      json_deinit(value);
+      return 1;
+    }
+
+    key.len = 0;
+    if (!s_push_cstr(&key, "message")) {
+      json_deinit(value);
+      return 1;
+    }
+
+    TaggedJsonValue_t *choices_0 =
+        da_get(TaggedJsonValue_t)(&choices->el.array, 0);
+
+    TaggedJsonValue_t *message =
+        hm_get(String_t, TaggedJsonValue_t)(&choices_0->el.object, &key);
+
+    if (!message) {
+      json_deinit(value);
+      return 1;
+    }
+
+    key.len = 0;
+    if (!s_push_cstr(&key, "content")) {
+      json_deinit(value);
+      return 1;
+    }
+
+    TaggedJsonValue_t *content =
+        hm_get(String_t, TaggedJsonValue_t)(&message->el.object, &key);
+    if (!content || content->type != JSON_STRING) {
+      json_deinit(value);
+      return 1;
+    }
+
+    String_t content_data = content->el.string;
+
+    for (size_t i = 0; i < content_data.len; i++)
+      putchar(content_data.buf[i]);
+
+    fflush(stdout);
+
+    s_deinit(&key);
+    json_deinit(value);
+  } else {
+    assert(0 && "not implemented yet");
+  }
+
+  return 0;
+}
+#endif
---
+++ jason/tests/json.c
@@ -0,0 +1,87 @@
+#include <assert.h>
+
+#define JSON_TESTS
+#include "../src/jason.c"
+
+void test_number() {
+  TaggedJsonValue_t json;
+  const char *json_data = "-123.22342348E-122";
+  Str_t json_data_slice = ss_from_cstring(json_data);
+  assert(json_parse(json_data_slice, &json) == 1 &&
+         "could not parse json string");
+  assert(json.type == JSON_NUMBER && "not a number ?!");
+  double expect = -123.22342348E-122;
+  assert(expect == json.el.number && "bad number");
+  json_deinit(json);
+}
+
+void test_boolean() {
+  TaggedJsonValue_t json;
+  const char *json_data = "false";
+  Str_t json_data_slice = ss_from_cstring(json_data);
+  assert(json_parse(json_data_slice, &json) == 1 &&
+         "could not parse json string");
+  assert(json.type == JSON_BOOL && "not a bool ?!");
+  assert(!json.el.boolean && "bad boolean");
+  json_deinit(json);
+}
+
+void test_string() {
+  TaggedJsonValue_t json;
+  const char *json_data =
+      "\"hello good world \\n \\r \\t              \\f\\\" \"";
+  Str_t json_data_slice = ss_from_cstring(json_data);
+  assert(json_parse(json_data_slice, &json) == 1 &&
+         "could not parse json string");
+  assert(json.type == JSON_STRING && "not a string ?!");
+  Str_t expect =
+      ss_from_cstring("hello good world \n \r \t              \f\" ");
+  assert(ss_eq(expect, s_str(&json.el.string)) && "bad string");
+  s_deinit(&json.el.string);
+  json_deinit(json);
+}
+
+void test_null() {
+  TaggedJsonValue_t json;
+  const char *json_data = "null";
+  Str_t json_data_slice = ss_from_cstring(json_data);
+  assert(json_parse(json_data_slice, &json) == 1 &&
+         "could not parse json string");
+  assert(json.type == JSON_NULL && "not a null?!");
+  json_deinit(json);
+}
+
+void test_array() {
+  TaggedJsonValue_t json;
+  const char *json_data =
+      "    [  1, 3 , \"dsfsdfvsdf \\\" \", true, false ,null     ,] \n \n\t ";
+  Str_t json_data_slice = ss_from_cstring(json_data);
+  assert(json_parse(json_data_slice, &json) == 1 &&
+         "could not parse json string");
+  assert(json.type == JSON_ARRAY && "not a array?!");
+  assert(json.el.array.len == 6 && "bad array length");
+  da_deinit(TaggedJsonValue_t)(&json.el.array, NULL);
+  json_deinit(json);
+}
+
+void test_object() {
+  TaggedJsonValue_t json;
+  const char *json_data = "{ \"1\": 1, \"2\": [1, 2, 3], \"3\": 123.3e-2, "
+                          "\"4\": {}, \"5\": true, \"6\": null, }";
+  Str_t json_data_slice = ss_from_cstring(json_data);
+  assert(json_parse(json_data_slice, &json) == 1 &&
+         "could not parse json string");
+  assert(json.type == JSON_OBJECT && "not a object?!");
+  assert(json.el.object.len == 6 && "bad array length");
+  json_deinit(json);
+}
+
+int main(void) {
+  test_string();
+  test_number();
+  test_boolean();
+  test_null();
+  test_array();
+  test_object();
+  return 0;
+}
